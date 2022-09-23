0eced4b306347e8b9290803ddcfaad1007a86eeb
Cho-D-YoungRae <yrc9229@gmail.com>
2022-09-23 12:49:12 +0900

    feat: Restaurant 도메인 수정
    카카오 에서 지역 데이터에 맞게

--- src/main/java/com/comeeatme/domain/restaurant/Restaurant.java
+++ src/main/java/com/comeeatme/domain/restaurant/Restaurant.java
@@ -1,6 +1,6 @@
 package com.comeeatme.domain.restaurant;
 
-import com.comeeatme.domain.core.BaseEntity;
+import com.comeeatme.domain.core.BaseTimeEntity;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -10,29 +10,20 @@ import javax.annotation.Nullable;
 import javax.persistence.*;
 
 @Entity
-@Table(name = "restaurant", uniqueConstraints =
-        @UniqueConstraint(name = "UK_restaurant_kakao_restaurant_id", columnNames = "kakao_restaurant_id")
-)
+@Table(name = "restaurant", uniqueConstraints = {
+        @UniqueConstraint(name = "UK_restaurant_open_management_num", columnNames = "open_management_num")
+})
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Restaurant extends BaseEntity {
+public class Restaurant extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "restaurant_id")
     private Long id;
 
-    @Column(name = "kakao_restaurant_id", nullable = false)
-    private Long kakaoId;
-
-    @Column(name = "kakao_place_url", nullable = false)
-    private String kakaoPlaceUrl;
-
     @Column(name = "name", length = 45, nullable = false)
     private String name;
-
-    @Column(name = "category_name", length = 45, nullable = false)
-    private String categoryName;
 
     @Column(name = "phone", length = 25, nullable = false)
     private String phone;
@@ -40,21 +31,20 @@ public class Restaurant extends BaseEntity {
     @Embedded
     private Address address;
 
+    @Embedded
+    private OpenInfo openInfo;
+
     @Builder
     private Restaurant(
             @Nullable Long id,
-            Long kakaoId,
-            String kakaoPlaceUrl,
             String name,
-            String categoryName,
             String phone,
-            Address address) {
+            Address address,
+            @Nullable OpenInfo openInfo) {
         this.id = id;
-        this.kakaoId = kakaoId;
-        this.kakaoPlaceUrl = kakaoPlaceUrl;
         this.name = name;
-        this.categoryName = categoryName;
         this.phone = phone;
         this.address = address;
+        this.openInfo = openInfo;
     }
 }
--- src/test/java/com/comeeatme/domain/restaurant/RestaurantTest.java
+++ src/test/java/com/comeeatme/domain/restaurant/RestaurantTest.java
@@ -9,6 +9,9 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 import static org.assertj.core.api.Assertions.assertThatNoException;
 
 @DataJpaTest
@@ -23,16 +26,21 @@ class RestaurantTest {
     @DisplayName("Restaurant 생성 및 저장")
     void save() {
         assertThatNoException().isThrownBy(() -> restaurantRepository.saveAndFlush(Restaurant.builder()
-                .kakaoId(25970354L)
-                .kakaoPlaceUrl("http://place.map.kakao.com/25970354")
                 .name("모노끼 야탑점")
-                .categoryName("음식점 > 일식 > 돈까스,우동")
                 .phone("031-702-2929")
                 .address(Address.builder()
                         .name("경기 성남시 분당구 야탑동 353-4")
                         .roadName("경기 성남시 분당구 야탑로69번길 24-6")
-                        .x(127.12729076428592)
-                        .y(37.41160407198509)
+                        .x(211199.96154825)
+                        .y(434395.793544651)
+                        .build())
+                .openInfo(OpenInfo.builder()
+                        .id("07_24_04_P")
+                        .name("일반음식점")
+                        .category("경양식")
+                        .managementNum("3810000-101-2002-00170")
+                        .permissionDate(LocalDate.of(2012, 12, 13))
+                        .lastModifiedAt(LocalDateTime.of(2022, 8, 31, 13, 13, 1))
                         .build())
                 .build()));
     }
