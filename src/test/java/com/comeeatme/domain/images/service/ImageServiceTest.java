5df1d446a5bb2dcfbb22d5d773387b3b6a83bfb7
Cho-D-YoungRae <yrc9229@gmail.com>
2022-10-02 03:31:38 +0900

    test: image id 리스트 검증 기능

--- src/test/java/com/comeeatme/domain/images/service/ImageServiceTest.java
+++ src/test/java/com/comeeatme/domain/images/service/ImageServiceTest.java
@@ -75,4 +75,90 @@ class ImageServiceTest {
                 .hasSize(1)
                 .containsExactly(1L);
     }
+
+    @Test
+    void validateImageIds() {
+        // given
+        List<Long> imageIds = List.of(1L, 2L, 3L);
+        Images image1 = mock(Images.class);
+        Images image2 = mock(Images.class);
+        Images image3 = mock(Images.class);
+
+        given(image1.getUseYn()).willReturn(true);
+        given(image2.getUseYn()).willReturn(true);
+        given(image3.getUseYn()).willReturn(true);
+
+        given(mockImagesRepository.findAllById(imageIds)).willReturn(List.of(image1, image2, image3));
+
+        Member member = mock(Member.class);
+        given(member.getId()).willReturn(1L);
+        given(member.getUseYn()).willReturn(true);
+
+        given(mockMemberRepository.findByUsername(anyString())).willReturn(Optional.of(member));
+
+        given(image1.getMember()).willReturn(member);
+        given(image2.getMember()).willReturn(member);
+        given(image3.getMember()).willReturn(member);
+
+        // expected
+        assertThat(imageService.validateImageIds(imageIds, "test-username")).isTrue();
+    }
+
+    @Test
+    void validateImageIds_DuplicatedImageIds() {
+        // given
+        List<Long> imageIds = List.of(1L, 2L, 2L);
+
+        // expected
+        assertThat(imageService.validateImageIds(imageIds, "test-username")).isFalse();
+    }
+
+    @Test
+    void validateImageIds_DeletedImageId() {
+        // given
+        List<Long> imageIds = List.of(1L, 2L, 3L);
+        Images image1 = mock(Images.class);
+        Images image2 = mock(Images.class);
+        Images image3 = mock(Images.class);
+
+        given(image1.getUseYn()).willReturn(true);
+        given(image2.getUseYn()).willReturn(true);
+        given(image3.getUseYn()).willReturn(false);
+
+        given(mockImagesRepository.findAllById(imageIds)).willReturn(List.of(image1, image2, image3));
+
+        // expected
+        assertThat(imageService.validateImageIds(imageIds, "test-username")).isFalse();
+    }
+
+    @Test
+    void validateImageIds_NotOwnedByMember() {
+        // given
+        List<Long> imageIds = List.of(1L, 2L, 3L);
+        Images image1 = mock(Images.class);
+        Images image2 = mock(Images.class);
+        Images image3 = mock(Images.class);
+
+        given(image1.getUseYn()).willReturn(true);
+        given(image2.getUseYn()).willReturn(true);
+        given(image3.getUseYn()).willReturn(true);
+
+        given(mockImagesRepository.findAllById(imageIds)).willReturn(List.of(image1, image2, image3));
+
+        Member member = mock(Member.class);
+        given(member.getId()).willReturn(1L);
+        given(member.getUseYn()).willReturn(true);
+
+        Member otherMember = mock(Member.class);
+        given(otherMember.getId()).willReturn(2L);
+
+        given(mockMemberRepository.findByUsername(anyString())).willReturn(Optional.of(member));
+
+        given(image1.getMember()).willReturn(member);
+        given(image2.getMember()).willReturn(member);
+        given(image3.getMember()).willReturn(otherMember);
+
+        // expected
+        assertThat(imageService.validateImageIds(imageIds, "test-username")).isFalse();
+    }
 }
