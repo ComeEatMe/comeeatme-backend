c17bb9a2a02d0f63534294b4d8d73f27cadb813d
Cho-D-YoungRae <yrc9229@gmail.com>
2022-10-02 20:41:37 +0900

    feat: RestaurantRepository.findAllSliceByNameStartingWith

--- src/main/java/com/comeeatme/domain/restaurant/repository/RestaurantRepository.java
+++ src/main/java/com/comeeatme/domain/restaurant/repository/RestaurantRepository.java
@@ -1,7 +1,11 @@
 package com.comeeatme.domain.restaurant.repository;
 
 import com.comeeatme.domain.restaurant.Restaurant;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
+
+    Slice<Restaurant> findAllSliceByNameStartingWith(Pageable pageable, String name);
 }
