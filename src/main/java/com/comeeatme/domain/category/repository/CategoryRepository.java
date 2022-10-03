a6fea1e19e65eed62323ddd3a670356c3e24ecb8
Cho-D-YoungRae <yrc9229@gmail.com>
2022-10-03 16:55:43 +0900

    prune: Category 도메인 제거

--- src/main/java/com/comeeatme/domain/category/repository/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.comeeatme.domain.category.repository;
-
-import com.comeeatme.domain.category.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
