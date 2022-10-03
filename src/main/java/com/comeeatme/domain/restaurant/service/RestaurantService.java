5a80b8e753f7857db926604534d8e7a516d0daed
Cho-D-YoungRae <yrc9229@gmail.com>
2022-10-03 14:48:24 +0900

    feat: RestaurantService 음식점 이름 및 주소 조회

---
+++ src/main/java/com/comeeatme/domain/restaurant/service/RestaurantService.java
@@ -0,0 +1,28 @@
+package com.comeeatme.domain.restaurant.service;
+
+import com.comeeatme.domain.restaurant.repository.RestaurantRepository;
+import com.comeeatme.domain.restaurant.response.RestaurantSimpleDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class RestaurantService {
+
+    private final RestaurantRepository restaurantRepository;
+
+    public Slice<RestaurantSimpleDto> getSimpleList(Pageable pageable, String name) {
+        return restaurantRepository.findSliceByNameStartingWith(pageable, name)
+                .map(restaurant -> RestaurantSimpleDto.builder()
+                        .id(restaurant.getId())
+                        .name(restaurant.getName())
+                        .addressName(restaurant.getAddress().getName())
+                        .build()
+                );
+    }
+
+}
