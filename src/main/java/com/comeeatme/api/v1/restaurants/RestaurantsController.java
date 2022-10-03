1a613decfe581c133a136740c97606680a29bf7e
Cho-D-YoungRae <yrc9229@gmail.com>
2022-10-03 16:43:59 +0900

    feat: 음식점 이름 및 주소 조회 API

---
+++ src/main/java/com/comeeatme/api/v1/restaurants/RestaurantsController.java
@@ -0,0 +1,29 @@
+package com.comeeatme.api.v1.restaurants;
+
+import com.comeeatme.api.common.dto.ApiResult;
+import com.comeeatme.domain.restaurant.response.RestaurantSimpleDto;
+import com.comeeatme.domain.restaurant.service.RestaurantService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/v1/restaurants")
+@RestController
+@RequiredArgsConstructor
+public class RestaurantsController {
+
+    private final RestaurantService restaurantService;
+
+    @GetMapping("/simple")
+    public ResponseEntity<ApiResult<Slice<RestaurantSimpleDto>>> getSimpleList(
+            Pageable pageable, @RequestParam String name) {
+        Slice<RestaurantSimpleDto> simpleList = restaurantService.getSimpleList(pageable, name);
+        ApiResult<Slice<RestaurantSimpleDto>> result = ApiResult.success(simpleList);
+        return ResponseEntity.ok(result);
+    }
+}
