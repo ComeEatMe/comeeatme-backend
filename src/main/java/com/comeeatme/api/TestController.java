ca8366035fb35b91450af16a3329161a44ad80f9
zooneon <snsxhdz@naver.com>
2022-10-04 23:05:20 +0900

    chore: 테스트 컨트롤러 추가
    파이프라인 테스트를 위해 테스트 컨트롤러 추가

---
+++ src/main/java/com/comeeatme/api/TestController.java
@@ -0,0 +1,13 @@
+package com.comeeatme.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/")
+    public String home() {
+        return "comeeatme api server";
+    }
+}
