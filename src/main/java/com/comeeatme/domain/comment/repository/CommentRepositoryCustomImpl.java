f6702c0c1d6a81fcc0f714f94159f98343dfb5c2
Cho-D-YoungRae <yrc9229@gmail.com>
2022-10-06 02:22:31 +0900

    feat: 댓글 리스트 조회

--- src/main/java/com/comeeatme/domain/comment/repository/CommentRepositoryCustomImpl.java
+++ src/main/java/com/comeeatme/domain/comment/repository/CommentRepositoryCustomImpl.java
@@ -1,14 +1,21 @@
 package com.comeeatme.domain.comment.repository;
 
+import com.comeeatme.domain.comment.Comment;
+import com.comeeatme.domain.post.Post;
 import com.querydsl.core.types.Expression;
 import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.SliceImpl;
 
+import java.util.List;
 import java.util.Optional;
 
 import static com.comeeatme.domain.account.QAccount.account;
 import static com.comeeatme.domain.comment.QComment.comment;
+import static com.comeeatme.domain.images.QImages.images;
 import static com.comeeatme.domain.member.QMember.member;
 
 @RequiredArgsConstructor
@@ -38,4 +45,25 @@ public class CommentRepositoryCustomImpl implements CommentRepositoryCustom {
                 .where(account.username.eq(username));
     }
 
+    @Override
+    public Slice<Comment> findSliceByPostWithMemberAndImage(Pageable pageable, Post post) {
+        List<Comment> content = query
+                .selectFrom(comment)
+                .join(comment.post)
+                .join(comment.member, member).fetchJoin()
+                .leftJoin(member.image, images).fetchJoin()
+                .where(comment.post.eq(post))
+                .orderBy(comment.parent.id.coalesce(comment.id).asc())
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize() + 1L)
+                .fetch();
+
+        boolean hasNext = false;
+        if (content.size() > pageable.getPageSize()) {
+            content.remove(pageable.getPageSize());
+            hasNext = true;
+        }
+
+        return new SliceImpl<>(content, pageable, hasNext);
+    }
 }
