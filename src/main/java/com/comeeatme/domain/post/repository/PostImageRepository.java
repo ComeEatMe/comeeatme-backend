9a79979fdec83503c9330f90c7d827f297270cb5
Cho-D-YoungRae <yrc9229@gmail.com>
2022-10-27 01:12:30 +0900

    feat: 게시물 이미지 조회

--- src/main/java/com/comeeatme/domain/post/repository/PostImageRepository.java
+++ src/main/java/com/comeeatme/domain/post/repository/PostImageRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface PostImageRepository extends JpaRepository<PostImage, Long> {
+public interface PostImageRepository extends JpaRepository<PostImage, Long>, PostImageRepositoryCustom {
 
     @EntityGraph(attributePaths = "image")
     List<PostImage> findAllWithImagesByPostInAndUseYnIsTrue(List<Post> posts);
