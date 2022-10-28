4e474297e3cc1ce7b714b168ca02fa2677a8051a
Cho-D-YoungRae <yrc9229@gmail.com>
2022-10-28 21:25:31 +0900

    feat: username 을 통한 memberId 반환

---
+++ src/main/java/com/comeeatme/domain/account/service/AccountService.java
@@ -0,0 +1,28 @@
+package com.comeeatme.domain.account.service;
+
+import com.comeeatme.domain.account.Account;
+import com.comeeatme.domain.account.repository.AccountRepository;
+import com.comeeatme.error.exception.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class AccountService {
+
+    private final AccountRepository accountRepository;
+
+    public Long getMemberId(String username) {
+        Account account = getAccountByUsername(username);
+        return account.getMember().getId();
+    }
+
+    private Account getAccountByUsername(String username) {
+        return accountRepository.findByUsername(username)
+                .filter(Account::getUseYn)
+                .orElseThrow(() -> new EntityNotFoundException("Account.username=" + username));
+    }
+
+}
