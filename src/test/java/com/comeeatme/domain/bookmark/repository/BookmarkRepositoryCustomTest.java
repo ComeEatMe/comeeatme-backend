5ee7d2d01b35faf0c968d692d4f63178ec6a4975
Cho-D-YoungRae <yrc9229@gmail.com>
2022-10-29 13:41:22 +0900

    test: 게시물 북마크 여부 조회 로직

---
+++ src/test/java/com/comeeatme/domain/bookmark/repository/BookmarkRepositoryCustomTest.java
@@ -0,0 +1,53 @@
+package com.comeeatme.domain.bookmark.repository;
+
+import com.comeeatme.common.TestJpaConfig;
+import com.comeeatme.domain.bookmark.Bookmark;
+import com.comeeatme.domain.member.Member;
+import com.comeeatme.domain.post.Post;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+@Transactional
+@Import(TestJpaConfig.class)
+class BookmarkRepositoryCustomTest {
+
+    @Autowired
+    private BookmarkRepository bookmarkRepository;
+
+    @Test
+    void findByMemberIdAndPostIds() {
+        // given
+        List<Bookmark> bookmarks = bookmarkRepository.saveAll(List.of(
+                Bookmark.builder()
+                        .member(Member.builder().id(10L).build())
+                        .post(Post.builder().id(1L).build())
+                        .build(),
+                Bookmark.builder()
+                        .member(Member.builder().id(10L).build())
+                        .post(Post.builder().id(2L).build())
+                        .build(),
+                Bookmark.builder()
+                        .member(Member.builder().id(11L).build())
+                        .post(Post.builder().id(3L).build())
+                        .build()
+        ));
+
+        // when
+        List<Bookmark> result = bookmarkRepository.findByMemberIdAndPostIds(10L, List.of(1L, 2L, 3L));
+
+        // then
+        result.sort((o1, o2) -> (int) (o1.getPost().getId() - o2.getPost().getId()));
+        assertThat(result)
+                .hasSize(2)
+                .extracting("id").containsExactly(bookmarks.get(0).getId(), bookmarks.get(1).getId());
+    }
+
+}
--- src/test/java/com/comeeatme/domain/bookmark/service/BookmarkServiceTest.java
+++ src/test/java/com/comeeatme/domain/bookmark/service/BookmarkServiceTest.java
@@ -5,6 +5,7 @@ import com.comeeatme.domain.bookmark.BookmarkGroup;
 import com.comeeatme.domain.bookmark.repository.BookmarkGroupRepository;
 import com.comeeatme.domain.bookmark.repository.BookmarkRepository;
 import com.comeeatme.domain.bookmark.response.BookmarkGroupDto;
+import com.comeeatme.domain.bookmark.response.PostBookmarked;
 import com.comeeatme.domain.member.Member;
 import com.comeeatme.domain.member.repository.MemberRepository;
 import com.comeeatme.domain.post.Post;
@@ -173,6 +174,32 @@ class BookmarkServiceTest {
         assertThat(result).hasSize(3);
         assertThat(result).extracting("name").containsExactly(BookmarkGroup.ALL_NAME, "그루비룸-1", "그루비룸-2");
         assertThat(result).extracting("bookmarkCount").containsExactly(10, 1, 2);
+    }
+
+    @Test
+    void isBookmarked() {
+        // given
+        Post post1 = mock(Post.class);
+        given(post1.getId()).willReturn(1L);
+        Bookmark bookmark1 = mock(Bookmark.class);
+        given(bookmark1.getPost()).willReturn(post1);
+
+        Post post2 = mock(Post.class);
+        given(post2.getId()).willReturn(2L);
+        Bookmark bookmark2 = mock(Bookmark.class);
+        given(bookmark2.getPost()).willReturn(post2);
+
+        List<Bookmark> bookmarks = List.of(bookmark1, bookmark2);
+        given(bookmarkRepository.findByMemberIdAndPostIds(3L, List.of(1L, 2L, 3L)))
+                .willReturn(bookmarks);
+
+        // when
+        List<PostBookmarked> result = bookmarkService.isBookmarked(3L, List.of(1L, 2L, 3L));
+
+        // then
+        assertThat(result).hasSize(3);
+        assertThat(result).extracting("postId").containsExactly(1L, 2L, 3L);
+        assertThat(result).extracting("bookmarked").containsExactly(true, true, false);
     }
 
 }
