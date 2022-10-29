c528dd975c36902ee55317ce6ab9d65385fe7266
Cho-D-YoungRae <yrc9229@gmail.com>
2022-10-29 13:45:41 +0900

    prune: 좋아요 여부 조회 API 삭제

--- src/main/java/com/comeeatme/api/v1/LikeController.java
+++ src/main/java/com/comeeatme/api/v1/LikeController.java
@@ -1,17 +1,10 @@
 package com.comeeatme.api.v1;
 
-import com.comeeatme.api.common.response.ApiResult;
-import com.comeeatme.domain.like.response.PostLiked;
 import com.comeeatme.domain.like.service.LikeService;
 import com.comeeatme.security.annotation.CurrentUsername;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
-import java.util.List;
 
 @RequestMapping("/v1")
 @RestController
@@ -30,14 +23,6 @@ public class LikeController {
     public ResponseEntity<Void> unlike(@PathVariable Long postId, @CurrentUsername String username) {
         likeService.unlike(postId, username);
         return ResponseEntity.noContent().build();
-    }
-
-    @GetMapping("/members/{memberId}/liked")
-    public ResponseEntity<ApiResult<List<PostLiked>>> getLiked(
-            @RequestParam @Valid @NotNull @Size(max = 100) List<Long> postIds, @PathVariable Long memberId) {
-        List<PostLiked> postLikeds = likeService.isLiked(memberId, postIds);
-        ApiResult<List<PostLiked>> result = ApiResult.success(postLikeds);
-        return ResponseEntity.ok(result);
     }
 
 }
--- src/test/java/com/comeeatme/api/v1/LikeControllerTest.java
+++ src/test/java/com/comeeatme/api/v1/LikeControllerTest.java
@@ -1,7 +1,6 @@
 package com.comeeatme.api.v1;
 
 import com.comeeatme.common.RestDocsConfig;
-import com.comeeatme.domain.like.response.PostLiked;
 import com.comeeatme.domain.like.service.LikeService;
 import com.comeeatme.security.SecurityConfig;
 import org.junit.jupiter.api.DisplayName;
@@ -14,25 +13,20 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.util.List;
-
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
 import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
 import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
-import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
-import static org.springframework.restdocs.payload.PayloadDocumentation.*;
-import static org.springframework.restdocs.request.RequestDocumentation.*;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.delete;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.put;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @Import(RestDocsConfig.class)
@@ -87,50 +81,6 @@ class LikeControllerTest {
                 ))
         ;
         then(likeService).should().unlike(eq(1L), anyString());
-    }
-
-    @Test
-    @WithMockUser
-    @DisplayName("게시물 좋아요 여부 조회 - 문서")
-    void getLiked_Docs() throws Exception {
-        // given
-        given(likeService.isLiked(3L, List.of(1L, 2L)))
-                .willReturn(List.of(
-                        PostLiked.builder()
-                                .postId(1L)
-                                .liked(true)
-                                .build(),
-                        PostLiked.builder()
-                                .postId(2L)
-                                .liked(false)
-                                .build()
-                ));
-
-        // expected
-        mockMvc.perform(get("/v1/members/{memberId}/liked", 3L)
-                        .header(HttpHeaders.AUTHORIZATION, "Bearer {ACCESS_TOKEN}")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .accept(MediaType.APPLICATION_JSON)
-                        .param("postIds", "1", "2")
-                )
-                .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.success").value(true))
-                .andDo(document("v1-like-get-liked",
-                        requestHeaders(
-                                headerWithName(HttpHeaders.AUTHORIZATION).description("인증 필요")
-                        ),
-                        requestParameters(
-                                parameterWithName("postIds")
-                                        .description("조회하려는 게시물 ID 리스트. 최대 100")
-                        ),
-                        responseFields(
-                                beneathPath("data[]").withSubsectionId("data"),
-                                fieldWithPath("postId").type(Long.class.getSimpleName()).description("게시물 ID"),
-                                fieldWithPath("liked").description("좋아요 여부")
-                        )
-                ))
-        ;
     }
 
 }
