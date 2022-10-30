6108554e1c620a85d271d354b239953ec0b95425
Cho-D-YoungRae <yrc9229@gmail.com>
2022-10-31 01:11:49 +0900

    feat: 좋아요한 게시물 조회

--- src/main/java/com/comeeatme/api/v1/LikeController.java
+++ src/main/java/com/comeeatme/api/v1/LikeController.java
@@ -1,10 +1,25 @@
 package com.comeeatme.api.v1;
 
+import com.comeeatme.api.common.response.ApiResult;
+import com.comeeatme.api.common.response.WithLikedBookmarked;
+import com.comeeatme.domain.account.service.AccountService;
+import com.comeeatme.domain.bookmark.response.PostBookmarked;
+import com.comeeatme.domain.bookmark.service.BookmarkService;
+import com.comeeatme.domain.like.response.LikedPostDto;
+import com.comeeatme.domain.like.response.PostLiked;
 import com.comeeatme.domain.like.service.LikeService;
 import com.comeeatme.security.annotation.CurrentUsername;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @RequestMapping("/v1")
 @RestController
@@ -12,6 +27,10 @@ import org.springframework.web.bind.annotation.*;
 public class LikeController {
 
     private final LikeService likeService;
+
+    private final AccountService accountService;
+
+    private final BookmarkService bookmarkService;
 
     @PutMapping("/member/like/{postId}")
     public ResponseEntity<Void> like(@PathVariable Long postId, @CurrentUsername String username) {
@@ -25,4 +44,32 @@ public class LikeController {
         return ResponseEntity.noContent().build();
     }
 
+    @GetMapping("/members/{memberId}/liked")
+    public ResponseEntity<ApiResult<Slice<WithLikedBookmarked<LikedPostDto>>>> getLikedList(
+            Pageable pageable, @PathVariable Long memberId, @CurrentUsername String username) {
+        Long myMemberId = accountService.getMemberId(username);
+        Slice<LikedPostDto> posts = likeService.getLikedPosts(pageable, memberId);
+        List<Long> postIds = posts.stream()
+                .map(LikedPostDto::getId)
+                .collect(Collectors.toList());
+        Set<Long> likedPostIds = Objects.equals(myMemberId, memberId) ?
+                new HashSet<>(postIds) :
+                likeService.areLiked(myMemberId, postIds).stream()
+                        .filter(PostLiked::getLiked)
+                        .map(PostLiked::getPostId)
+                        .collect(Collectors.toSet());
+        Set<Long> bookmarkedPostIds = bookmarkService.areBookmarked(myMemberId, postIds).stream()
+                .filter(PostBookmarked::getBookmarked)
+                .map(PostBookmarked::getPostId)
+                .collect(Collectors.toSet());
+        Slice<WithLikedBookmarked<LikedPostDto>> postWiths = posts
+                .map(post -> WithLikedBookmarked.<LikedPostDto>builder()
+                        .post(post)
+                        .liked(likedPostIds.contains(post.getId()))
+                        .bookmarked(bookmarkedPostIds.contains(post.getId()))
+                        .build()
+                );
+        ApiResult<Slice<WithLikedBookmarked<LikedPostDto>>> apiResult = ApiResult.success(postWiths);
+        return ResponseEntity.ok(apiResult);
+    }
 }
---
+++ src/main/java/com/comeeatme/domain/like/response/LikedPostDto.java
@@ -0,0 +1,21 @@
+package com.comeeatme.domain.like.response;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class LikedPostDto {
+
+    private Long id;
+
+    private List<String> imageUrls;
+
+    private String content;
+
+    private LocalDateTime createdAt;
+}
