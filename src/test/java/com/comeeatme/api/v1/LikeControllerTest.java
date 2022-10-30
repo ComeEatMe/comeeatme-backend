4ecd51240fa34c7ef3a9093abb1c29879042ef73
Cho-D-YoungRae <yrc9229@gmail.com>
2022-10-31 01:12:01 +0900

    test: 좋아요한 게시물 조회

--- src/test/java/com/comeeatme/api/v1/LikeControllerTest.java
+++ src/test/java/com/comeeatme/api/v1/LikeControllerTest.java
@@ -1,6 +1,11 @@
 package com.comeeatme.api.v1;
 
 import com.comeeatme.common.RestDocsConfig;
+import com.comeeatme.domain.account.service.AccountService;
+import com.comeeatme.domain.bookmark.response.PostBookmarked;
+import com.comeeatme.domain.bookmark.service.BookmarkService;
+import com.comeeatme.domain.like.response.LikedPostDto;
+import com.comeeatme.domain.like.response.PostLiked;
 import com.comeeatme.domain.like.service.LikeService;
 import com.comeeatme.security.SecurityConfig;
 import org.junit.jupiter.api.DisplayName;
@@ -12,21 +17,28 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.SliceImpl;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.ArgumentMatchers.eq;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
 import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
 import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
-import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.delete;
-import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.put;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
 import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @Import(RestDocsConfig.class)
@@ -40,6 +52,12 @@ class LikeControllerTest {
 
     @MockBean
     private LikeService likeService;
+
+    @MockBean
+    private AccountService accountService;
+
+    @MockBean
+    private BookmarkService bookmarkService;
 
     @Test
     @WithMockUser
@@ -64,7 +82,7 @@ class LikeControllerTest {
 
     @Test
     @WithMockUser
-    @DisplayName("게시물 좋아요 취소 - 문서")
+    @DisplayName("게시물 좋아요 취소 - DOCS")
     void unlike_Docs() throws Exception {
         // expected
         mockMvc.perform(delete("/v1/member/like/{postId}", 1L)
@@ -81,6 +99,103 @@ class LikeControllerTest {
                 ))
         ;
         then(likeService).should().unlike(eq(1L), anyString());
+    }
+
+    @Test
+    @WithMockUser
+    @DisplayName("좋아요한 게시물 조회 - DOCS")
+    void getLikedList() throws Exception {
+        // given
+        given(accountService.getMemberId(anyString())).willReturn(1L);
+
+        LikedPostDto likedPostDto = LikedPostDto.builder()
+                .id(2L)
+                .content("post-content")
+                .createdAt(LocalDateTime.of(2022, 10, 31, 0, 53))
+                .imageUrls(List.of("image-url-1", "image-url-2"))
+                .build();
+        given(likeService.getLikedPosts(any(Pageable.class), eq(1L)))
+                .willReturn(new SliceImpl<>(List.of(likedPostDto)));
+
+        PostBookmarked postBookmarked = PostBookmarked.builder()
+                .postId(2L)
+                .bookmarked(false)
+                .build();
+        given(bookmarkService.areBookmarked(1L, List.of(2L)))
+                .willReturn(List.of(postBookmarked));
+
+        // expected
+        mockMvc.perform(get("/v1/members/{memberId}/liked", 1L)
+                        .header(HttpHeaders.AUTHORIZATION, "Bearer {ACCESS_TOKEN}")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.success").value(true))
+                .andExpect(jsonPath("$.data.content[0].liked").value(true))
+                .andExpect(jsonPath("$.data.content[0].bookmarked").value(false))
+                .andDo(document("v1-like-get-liked-list",
+                        requestHeaders(
+                                headerWithName(HttpHeaders.AUTHORIZATION).description("인증 필요")
+                        ),
+                        pathParameters(
+                                parameterWithName("memberId").description("회원 ID")
+                        ),
+                        responseFields(
+                                beneathPath("data.content[]").withSubsectionId("content"),
+                                fieldWithPath("id").type(Long.class.getSimpleName()).description("게시물 ID"),
+                                fieldWithPath("imageUrls").description("게시물 이미지 URL 리스트"),
+                                fieldWithPath("content").description("게시물 내용"),
+                                fieldWithPath("createdAt").description("게시물 생성 시점"),
+                                fieldWithPath("liked").description("좋아요 여부"),
+                                fieldWithPath("bookmarked").description("북마크 여부")
+                        )
+                ))
+        ;
+    }
+
+    @Test
+    @WithMockUser
+    @DisplayName("좋아요한 게시물 조회 - 본인 이회 회원 조회")
+    void getLikedList_MemberIdNotEqual() throws Exception {
+        // given
+        long memberId = 1L;
+        long myMemberId = 5L;
+        given(accountService.getMemberId(anyString())).willReturn(myMemberId);
+
+        LikedPostDto likedPostDto = LikedPostDto.builder()
+                .id(2L)
+                .content("post-content")
+                .createdAt(LocalDateTime.of(2022, 10, 31, 0, 53))
+                .imageUrls(List.of("image-url-1", "image-url-2"))
+                .build();
+        given(likeService.getLikedPosts(any(Pageable.class), eq(memberId)))
+                .willReturn(new SliceImpl<>(List.of(likedPostDto)));
+
+        PostLiked postLiked = PostLiked.builder()
+                .postId(2L)
+                .liked(false)
+                .build();
+        given(likeService.areLiked(myMemberId, List.of(2L)))
+                .willReturn(List.of(postLiked));
+
+        PostBookmarked postBookmarked = PostBookmarked.builder()
+                .postId(2L)
+                .bookmarked(true)
+                .build();
+        given(bookmarkService.areBookmarked(myMemberId, List.of(2L)))
+                .willReturn(List.of(postBookmarked));
+
+
+        // expected
+        mockMvc.perform(get("/v1/members/{memberId}/liked", memberId)
+                        .header(HttpHeaders.AUTHORIZATION, "Bearer {ACCESS_TOKEN}")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.success").value(true))
+                .andExpect(jsonPath("$.data.content[0].liked").value(false))
+                .andExpect(jsonPath("$.data.content[0].bookmarked").value(true))
+        ;
     }
 
 }
--- src/test/java/com/comeeatme/domain/like/service/LikeServiceTest.java
+++ src/test/java/com/comeeatme/domain/like/service/LikeServiceTest.java
@@ -1,11 +1,15 @@
 package com.comeeatme.domain.like.service;
 
+import com.comeeatme.domain.image.Image;
 import com.comeeatme.domain.like.Like;
 import com.comeeatme.domain.like.repository.LikeRepository;
+import com.comeeatme.domain.like.response.LikedPostDto;
 import com.comeeatme.domain.like.response.PostLiked;
 import com.comeeatme.domain.member.Member;
 import com.comeeatme.domain.member.repository.MemberRepository;
 import com.comeeatme.domain.post.Post;
+import com.comeeatme.domain.post.PostImage;
+import com.comeeatme.domain.post.repository.PostImageRepository;
 import com.comeeatme.domain.post.repository.PostRepository;
 import com.comeeatme.error.exception.AlreadyLikedPostException;
 import com.comeeatme.error.exception.EntityNotFoundException;
@@ -15,12 +19,19 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.SliceImpl;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.mock;
@@ -32,13 +43,16 @@ class LikeServiceTest {
     private LikeService likeService;
 
     @Mock
-    private LikeRepository likesRepository;
+    private LikeRepository likeRepository;
 
     @Mock
     private PostRepository postRepository;
 
     @Mock
     private MemberRepository memberRepository;
+
+    @Mock
+    private PostImageRepository postImageRepository;
 
     @Test
     void like() {
@@ -51,14 +65,14 @@ class LikeServiceTest {
         given(member.getUseYn()).willReturn(true);
         given(memberRepository.findByUsername("username")).willReturn(Optional.of(member));
 
-        given(likesRepository.existsByPostAndMember(post, member)).willReturn(false);
+        given(likeRepository.existsByPostAndMember(post, member)).willReturn(false);
 
         // when
         likeService.like(1L, "username");
 
         // then
         ArgumentCaptor<Like> likesCaptor = ArgumentCaptor.forClass(Like.class);
-        then(likesRepository).should().save(likesCaptor.capture());
+        then(likeRepository).should().save(likesCaptor.capture());
         Like likeCaptorValue = likesCaptor.getValue();
         assertThat(likeCaptorValue.getPost()).isEqualTo(post);
         assertThat(likeCaptorValue.getMember()).isEqualTo(member);
@@ -76,7 +90,7 @@ class LikeServiceTest {
         given(member.getUseYn()).willReturn(true);
         given(memberRepository.findByUsername("username")).willReturn(Optional.of(member));
 
-        given(likesRepository.existsByPostAndMember(post, member)).willReturn(true);
+        given(likeRepository.existsByPostAndMember(post, member)).willReturn(true);
 
         // expected
         assertThatThrownBy(() -> likeService.like(1L, "username"))
@@ -95,13 +109,13 @@ class LikeServiceTest {
         given(memberRepository.findByUsername("username")).willReturn(Optional.of(member));
 
         Like like = mock(Like.class);
-        given(likesRepository.findByPostAndMember(post, member)).willReturn(Optional.of(like));
+        given(likeRepository.findByPostAndMember(post, member)).willReturn(Optional.of(like));
 
         // when
         likeService.unlike(1L, "username");
 
         // then
-        then(likesRepository).should().delete(like);
+        then(likeRepository).should().delete(like);
     }
 
     @Test
@@ -116,7 +130,7 @@ class LikeServiceTest {
         given(member.getUseYn()).willReturn(true);
         given(memberRepository.findByUsername("username")).willReturn(Optional.of(member));
 
-        given(likesRepository.findByPostAndMember(post, member)).willReturn(Optional.empty());
+        given(likeRepository.findByPostAndMember(post, member)).willReturn(Optional.empty());
 
         // expected
         assertThatThrownBy(() -> likeService.unlike(1L, "username"))
@@ -137,7 +151,7 @@ class LikeServiceTest {
         given(like2.getPost()).willReturn(post2);
 
         List<Like> likes = List.of(like1, like2);
-        given(likesRepository.findByMemberIdAndPostIds(3L, List.of(1L, 2L, 3L)))
+        given(likeRepository.findByMemberIdAndPostIds(3L, List.of(1L, 2L, 3L)))
                 .willReturn(likes);
 
         // when
@@ -147,6 +161,55 @@ class LikeServiceTest {
         assertThat(result).hasSize(3);
         assertThat(result).extracting("postId").containsExactly(1L, 2L, 3L);
         assertThat(result).extracting("liked").containsExactly(true, true, false);
+    }
+
+    @Test
+    void getLikedPosts() {
+        // given
+        Member member = mock(Member.class);
+        given(member.getUseYn()).willReturn(true);
+        given(memberRepository.findById(1L)).willReturn(Optional.of(member));
+
+        Post post = mock(Post.class);
+        given(post.getId()).willReturn(2L);
+        given(post.getContent()).willReturn("content");
+        given(post.getCreatedAt()).willReturn(LocalDateTime.of(2022, 10, 31, 0, 21));
+
+        Like like = mock(Like.class);
+        given(like.getPost()).willReturn(post);
+        given(likeRepository.findSliceWithPostByMember(any(Pageable.class), eq(member)))
+                .willReturn(new SliceImpl<>(List.of(like)));
+
+        Image image1 = mock(Image.class);
+        given(image1.getUseYn()).willReturn(true);
+        given(image1.getUrl()).willReturn("url-1");
+        PostImage postImage1 = mock(PostImage.class);
+        given(postImage1.getImage()).willReturn(image1);
+        given(postImage1.getPost()).willReturn(post);
+
+        Image image2 = mock(Image.class);
+        given(image2.getUseYn()).willReturn(false);
+        PostImage postImage2 = mock(PostImage.class);
+        given(postImage2.getImage()).willReturn(image2);
+
+        given(postImageRepository.findAllWithImageByPostIn(List.of(post)))
+                .willReturn(List.of(postImage1, postImage2));
+
+        // when
+        PageRequest pageRequest = PageRequest.of(0, 10);
+        Slice<LikedPostDto> result = likeService.getLikedPosts(pageRequest, 1L);
+
+        // then
+        List<LikedPostDto> content = result.getContent();
+        assertThat(content).hasSize(1);
+        assertThat(content).extracting("id").containsExactly(2L);
+        assertThat(content).extracting("content").containsExactly("content");
+        assertThat(content).extracting("createdAt").containsExactly(LocalDateTime.of(2022, 10, 31, 0, 21));
+
+        List<String> imageUrls = content.get(0).getImageUrls();
+        assertThat(imageUrls)
+                .hasSize(1)
+                .containsExactly("url-1");
     }
 
 }
