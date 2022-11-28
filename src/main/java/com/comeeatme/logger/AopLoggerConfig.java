d5b904b93f390a5766eae3dd15d58e198ab08b12
Cho-D-YoungRae <yrc9229@gmail.com>
2022-11-28 14:25:20 +0900

    feat: 성능 로깅 AOP

---
+++ src/main/java/com/comeeatme/logger/AopLoggerConfig.java
@@ -0,0 +1,16 @@
+package com.comeeatme.logger;
+
+import com.comeeatme.logger.trace.logtrace.ThreadLocalLogTrace;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Profile({"local", "dev"})
+@Configuration
+public class AopLoggerConfig {
+
+    @Bean
+    public LogTraceAspect logTraceAspect() {
+        return new LogTraceAspect(new ThreadLocalLogTrace());
+    }
+}
---
+++ src/main/java/com/comeeatme/logger/trace/TraceStatus.java
@@ -0,0 +1,13 @@
+package com.comeeatme.logger.trace;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class TraceStatus {
+
+    private final TraceId traceId;
+    private final long startTimeMs;
+    private final String message;
+}
