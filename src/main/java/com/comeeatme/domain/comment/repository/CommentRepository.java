54be694cfd295479ef186063fe632c04bd7c4a80
Cho-D-YoungRae <yrc9229@gmail.com>
2022-12-09 00:27:28 +0900

    feat: CommentService Account 의존성 제거

--- src/main/java/com/comeeatme/domain/comment/repository/CommentRepository.java
+++ src/main/java/com/comeeatme/domain/comment/repository/CommentRepository.java
@@ -1,6 +1,7 @@
 package com.comeeatme.domain.comment.repository;
 
 import com.comeeatme.domain.comment.Comment;
+import com.comeeatme.domain.member.Member;
 import com.comeeatme.domain.post.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -11,5 +12,7 @@ public interface CommentRepository extends JpaRepository<Comment, Long>, Comment
     boolean existsByIdAndPostAndUseYnIsTrue(Long id, Post post);
 
     List<Comment> findAllByPostAndUseYnIsTrue(Post post);
+
+    boolean existsByIdAndMember(Long id, Member member);
 
 }
--- src/main/java/com/comeeatme/domain/comment/repository/CommentRepositoryCustomImpl.java
+++ src/main/java/com/comeeatme/domain/comment/repository/CommentRepositoryCustomImpl.java
@@ -2,8 +2,6 @@ package com.comeeatme.domain.comment.repository;
 
 import com.comeeatme.domain.comment.Comment;
 import com.comeeatme.domain.post.Post;
-import com.querydsl.core.types.Expression;
-import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
@@ -11,39 +9,15 @@ import org.springframework.data.domain.Slice;
 import org.springframework.data.domain.SliceImpl;
 
 import java.util.List;
-import java.util.Optional;
 
 import static com.comeeatme.domain.comment.QComment.comment;
 import static com.comeeatme.domain.image.QImage.image;
 import static com.comeeatme.domain.member.QMember.member;
-import static com.comeeatme.security.account.QAccount.account;
 
 @RequiredArgsConstructor
 public class CommentRepositoryCustomImpl implements CommentRepositoryCustom {
 
     private final JPAQueryFactory query;
-
-    @Override
-    public boolean existsByIdAndUsernameAndUseYnIsTrue(Long commentId, String username) {
-        return Optional.ofNullable(query
-                        .selectOne()
-                        .from(comment)
-                        .join(comment.member, member)
-                        .where(
-                                comment.id.eq(commentId),
-                                comment.useYn.isTrue(),
-                                member.id.eq(memberIdOfUsername(username)))
-                        .fetchOne())
-                .isPresent();
-    }
-
-    private Expression<Long> memberIdOfUsername(String username) {
-        return JPAExpressions
-                .select(member.id)
-                .from(account)
-                .join(account.member, member)
-                .where(account.username.eq(username));
-    }
 
     @Override
     public Slice<Comment> findSliceByPostWithMemberAndImage(Pageable pageable, Post post) {
