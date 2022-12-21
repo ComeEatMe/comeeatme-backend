47182569beb5941fd5ea4734ad5a9d75ce18320e
Cho-D-YoungRae <yrc9229@gmail.com>
2022-12-22 02:45:12 +0900

    feat: 공지사항 리스트 조회

---
+++ src/main/java/com/comeeatme/domain/notice/service/NoticeService.java
@@ -0,0 +1,28 @@
+package com.comeeatme.domain.notice.service;
+
+import com.comeeatme.domain.notice.repository.NoticeRepository;
+import com.comeeatme.domain.notice.response.NoticeDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class NoticeService {
+
+    private final NoticeRepository noticeRepository;
+
+    public Slice<NoticeDto> getList(Pageable pageable) {
+        return noticeRepository.findAll(pageable)
+                .map(notice -> NoticeDto.builder()
+                        .type(notice.getType())
+                        .title(notice.getTitle())
+                        .content(notice.getContent())
+                        .createdAt(notice.getCreatedAt())
+                        .build()
+                );
+    }
+}
